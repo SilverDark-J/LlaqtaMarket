3fa6bb52d5515277169e05da88f210f8ffb35013
SilverDark-J <silverdark911@gmail.com>
2025-04-29 22:41:51 -0500

    Diseño pricipal del index, con estilos y scripts

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+function menuDeslizable() {
+
+  const menu = document.getElementById("menuOpciones");
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+
+}
+
