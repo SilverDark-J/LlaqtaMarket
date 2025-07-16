928cb6bff038be4eac15357ec7607f7d06b0b7ed
SilverDark-J <silverdark911@gmail.com>
2025-07-16 03:50:03 -0500

    Corregir funcionalidad del header y opciones para agregar productos del emprendedor

--- frontend-react/src/layouts/PublicLayout.jsx
+++ frontend-react/src/layouts/PublicLayout.jsx
@@ -3,10 +3,10 @@ import PublicHeader from "../components/PublicHeader";
 import PublicFooter from "../components/PublicFooter";
 import styles from "../styles/publicLayout.module.css";
 
-export default function PublicLayout({ children }) {
+export default function PublicLayout({ children, onBuscar }) {
   return (
     <div className={styles.layout}>
-      <PublicHeader mostrarCategorias={true} />
+      <PublicHeader mostrarCategorias={true} onBuscar={onBuscar} />
       <main className={styles.main}>{children}</main>
       <PublicFooter />
     </div>
